f55a7cd3c64fa90da1875035047a42731b111d6a
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-02-23 13:22:46 +0530

    merging both parts

--- lab3/lab3_1.R
+++ lab3/lab3_1.R
@@ -20,3 +20,18 @@ if(p<0.01){
   cat ("\nclaim can not be rejected for 0.01")
 }
 #q =qnorm(0.05,m,sd) ## what can be the actual value 
+#case 2 
+se <- 17/sqrt(35)
+cat ("\nfor second case \nStandard error is :",se)
+p <-pnorm(134,130,se,lower.tail = F)*2
+cat ("\nP value is :",p)
+if(p<0.05){
+  cat ("\nclaim can be rejected for 0.05")
+}else{
+  cat ("\nclaim can not be rejected for 0.05")
+}
+if(p<0.01){
+  cat ("\nclaim can be rejected for 0.01")
+}else{
+  cat ("\nclaim can not be rejected for 0.01")
+}
--- lab3/lab3_2.R
+++
@@ -1,5 +0,0 @@
-#case 2 
-se <- 17/sqrt(35)
-cat ("Standard error is :",se)
-p <-pnorm(134,130,se,lower.tail = F)
-cat ("P value is :",p)
