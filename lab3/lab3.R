9916f15b96634b24c051250df316ea9fdfb1c0c8
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-02-23 12:46:03 +0530

    lab 3 1st part done

--- lab3/lab3.R
+++ lab3/lab3.R
@@ -1,1 +1,23 @@
 f <- read.csv("Hypothesis_csv1.csv")
+
+#first case 
+
+m  <-mean(f$Life_Hrs)
+cat ("mean observed is :",m)
+sd<-sd(f$Life_Hrs)
+se <-sd/sqrt(50)
+cat ("\nstandard error is :",se)
+p <-pnorm(m,10000,se)
+cat ("\npvalue for the 1st case is :",p)
+if(p<0.05){
+  cat ("\nclaim can be rejected for 0.05")
+}else{
+  cat ("\nclaim can not be rejected for 0.05")
+}
+if(p<0.01){
+  cat ("\nclaim can be rejected for 0.01")
+}else{
+  cat ("\nclaim can not be rejected for 0.01")
+}
+#q =qnorm(0.05,m,sd) ## what can be the actual  would be  
+
