71b9e108340c7b081a9eab48e4e9ed87bf3dc5b2
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-05-04 22:28:44 +0530

    add graph

--- ha/ha.R
+++ ha/ha.R
@@ -3,37 +3,57 @@ set.seed(123)
 library(caret)
 library(randomForest)
 library(dplyr)
-library(e1071)
-library(GGally)
 
-str(wine)
-
-
-wine<-wine[ ,-1]
-q<-ggcorr(wine, label = F)
+q<-ggcorr(wine, label = T)
 print(q)
-trainIndex <- createDataPartition(wine$quality, p = 0.8, list = FALSE)
-trainData <- wine[trainIndex, ]
-testData <- wine[-trainIndex, ]
-test.x <- testData[,-12]
+str(wine)
+wine<-wine[ ,-c(1,2,7)]
+wine$quality<-as.factor(wine$quality)
 
-mlr<-lm(quality~.,trainData)
-s1<-summary(mlr)
-print(s1)
-
-svm_model <- svm(quality ~ ., data=trainData, kernel="linear", cost=10)
-predictio <- predict(svm_model, newdata=test.x)
-predictions <- round(predictio,digits = 0)
-
-confusion_mat <- table(testData$quality, predictions)
-accuracy <- sum(diag(confusion_mat))/sum(confusion_mat)
-precision <- confusion_mat[2,2]/sum(confusion_mat[,2])
-recall <- confusion_mat[2,2]/sum(confusion_mat[2,])
-f1_score <- 2*(precision*recall)/(precision+recall)
+set.seed(123)
 
 
-cat("Accuracy:", accuracy, "\n")
-cat("Precision:", precision, "\n")
-cat("Recall:", recall, "\n")
-cat("F1-score:", f1_score, "\n")
+train <- sample(nrow(wine), 0.8 * nrow(wine))
+train_data <- wine[train, ]
+test_data <- wine[-train, ]
 
+#Random Forest 
+model <- randomForest(as.factor(quality) ~ ., data = train_data, ntree = 1000)
+
+predictions <- predict(model, test_data)
+
+conf_matrix <- table(test_data$quality, predictions)
+print(conf_matrix)
+
+
+accuracy_rf <- sum(diag(conf_matrix)) / sum(conf_matrix)
+cat("\nAccuracy of Random Forest:", accuracy_rf, "\n")
+
+# Naive Bayes
+model_nb <- naiveBayes(quality ~ ., data = train_data)
+
+nb_predictions <- predict(model_nb, newdata = test_data)
+confusion_matrix <- table(test_data$quality, nb_predictions)
+print(confusion_matrix)
+
+accuracy_nb <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+cat("\nAccuracy of Naive Bayes:", accuracy_nb, "\n")
+
+#SVM
+svm_model <- svm(quality ~ ., data = train_data, kernel = "linear", cost = 10)
+
+svm_pred <- predict(svm_model, newdata = test_data)
+
+svm_cm <- table(svm_pred, test_data$quality)
+print(svm_cm)
+accuracy_svm <- sum(diag(conf_matrix)) / sum(conf_matrix)
+cat("\nAccuracy of SVM: ",accuracy_svm)
+
+rf_metrics <- caret::confusionMatrix(predictions, test_data$quality)
+print(rf_metrics)
+
+nb_metrics <- caret::confusionMatrix(nb_predictions, test_data$quality)
+print(nb_metrics)
+
+svm_metrics <- caret::confusionMatrix(svm_pred, test_data$quality)
+print(svm_metrics)
