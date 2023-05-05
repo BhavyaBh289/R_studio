d513a96b4ee0763974fc607e6bcd0cc87e8491d2
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-05-05 09:45:53 +0530

    reforging code

--- ha/ha.R
+++ ha/ha.R
@@ -3,11 +3,13 @@ set.seed(123)
 library(caret)
 library(randomForest)
 library(dplyr)
+library(GGally)
+library(e1071)
 
 wine<-wine[ ,-c(1)]
+str(wine)
 q<-ggcorr(wine, label = T)
 print(q)
-str(wine)
 wine<-wine[ ,-c(1,6)]
 wine$quality<-as.factor(wine$quality)
 
@@ -28,7 +30,7 @@ print(conf_matrix)
 
 
 accuracy_rf <- sum(diag(conf_matrix)) / sum(conf_matrix)
-cat("\nAccuracy of Random Forest:", accuracy_rf, "\n")
+# cat("\nAccuracy of Random Forest:", accuracy_rf, "\n")
 
 # Naive Bayes
 model_nb <- naiveBayes(quality ~ ., data = train_data)
@@ -38,7 +40,7 @@ confusion_matrix <- table(test_data$quality, nb_predictions)
 print(confusion_matrix)
 
 accuracy_nb <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
-cat("\nAccuracy of Naive Bayes:", accuracy_nb, "\n")
+# cat("\nAccuracy of Naive Bayes:", accuracy_nb, "\n")
 
 #SVM
 svm_model <- svm(quality ~ ., data = train_data, kernel = "linear", cost = 10)
@@ -47,8 +49,12 @@ svm_pred <- predict(svm_model, newdata = test_data)
 
 svm_cm <- table(svm_pred, test_data$quality)
 print(svm_cm)
-accuracy_svm <- sum(diag(conf_matrix)) / sum(conf_matrix)
-cat("\nAccuracy of SVM: ",accuracy_svm)
+accuracy_svm <- sum(diag(svm_cm)) / sum(svm_cm)
+# cat("\nAccuracy of SVM: ",accuracy_svm)
+
+H <- c(accuracy_nb*100,accuracy_svm*100,accuracy_rf*100)
+M <- c("Naive Bayes","SVM","Random Forest")
+barplot(H,names.arg=M,xlab="model",ylab="Accuracy",col="blue", main="Accuracy chart",border="red")
 
 rf_metrics <- caret::confusionMatrix(predictions, test_data$quality)
 print(rf_metrics)
@@ -58,9 +64,3 @@ print(nb_metrics)
 
 svm_metrics <- caret::confusionMatrix(svm_pred, test_data$quality)
 print(svm_metrics)
-
-
-
-H <- c(accuracy_nb*100,accuracy_rf*100,accuracy_svm*100)
-M <- c("Naive Bayes","Random Forest","SVM")
-barplot(H,names.arg=M,xlab="model",ylab="Accuracy",col="blue", main="Accuracy chart",border="red")
